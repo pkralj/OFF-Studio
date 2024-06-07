b41f49f4c9e92afb58b8fc89ee144cfc1fe3b9e6
Patrik Kralj <patrikkralj97@gmail.com>
2024-06-07 18:49:27 +0200

    added white poster for better view of video parts, changed test to different values of blur to check which is best

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function() {
     document.getElementById("footer").style.display = "flex";
-  }, 5000);
+  }, 2000);
   
   window.addEventListener("scroll", function() {
     document.getElementById("footer").style.opacity = "0";
