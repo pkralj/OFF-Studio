6735f4e6302a6717ccadd12acba164d88d801f0d
Patrik Kralj <patrikkralj97@gmail.com>
2024-06-12 17:02:34 +0200

    fixed z-index issues, added js function to remove borders from fullscreen videos, cleaned up some residual code

--- index.js
+++ index.js
@@ -6,5 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("scroll", function() {
     document.getElementById("footer").style.opacity = "0";
   });
-
 });
+
+const videos = document.querySelectorAll('.video-content');
+
+function toggleBorder () {
+  if(document.fullscreenElement) {
+    videos.forEach(video => video.classList.add('no-border'));
+  } else {
+    videos.forEach(video => video.classList.remove('no-border'));
+  }
+}
+
+
+
+document.addEventListener('fullscreenchange', toggleBorder);
+document.addEventListener('webkitfullscreenchange', toggleBorder);
+document.addEventListener('mozfullscreenchange', toggleBorder);
+document.addEventListener('msfullscreenchange', toggleBorder);
