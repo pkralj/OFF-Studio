f9a32ebec20a400d885853baef059b71e7fcbd72
Patrik Kralj <patrikkralj97@gmail.com>
2024-06-13 11:46:47 +0200

    Added a contact page and its .js and .css files, added fadeout and fadein animations for switching between both, fixed some more stuff

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const overlay = document.querySelector('.empty-div');
+  const logoVideo = document.getElementById('logo-video');
+
+  logoVideo.addEventListener('loadeddata', function() {
+    setTimeout(() => {
+      overlay.classList.add('fadeOut');
+      setTimeout(() => {
+        overlay.style.zIndex = '-4';
+      }, 500);
+    },0 );
+  });
+  
+});
+
+
 document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function() {
     document.getElementById("footer").style.display = "flex";
-  }, 2000);
+  }, 5000);
   
   window.addEventListener("scroll", function() {
     document.getElementById("footer").style.opacity = "0";
@@ -17,10 +33,15 @@ function toggleBorder () {
     videos.forEach(video => video.classList.remove('no-border'));
   }
 }
-
-
-
 document.addEventListener('fullscreenchange', toggleBorder);
 document.addEventListener('webkitfullscreenchange', toggleBorder);
 document.addEventListener('mozfullscreenchange', toggleBorder);
 document.addEventListener('msfullscreenchange', toggleBorder);
+
+document.getElementById('nav-contact').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.body.classList.add('fadeOut');
+  setTimeout(function () {
+    window.location.href = 'contact/contact.html';
+  }, 500);
+});
