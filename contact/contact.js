80cf84ca51c4d8fc5d3900d50e2ffd7e6b0b2a7b
Patrik Kralj <patrikkralj97@gmail.com>
2024-06-14 12:22:40 +0200

    Added the final video and posters, started making a form, and added navigation to and from the form, from the contact page via email link

--- contact/contact.js
+++ contact/contact.js
@@ -1,6 +1,15 @@
 document.getElementById('x-button').addEventListener('click', function() {
-    document.body.classList.add('fadeOut');
-    setTimeout(function () {
-      window.location.href = '../index.html';
-    }, 500);
-  });
+  document.body.classList.add('fadeOut');
+  setTimeout(function () {
+    window.location.href = '../index.html';
+  }, 500);
+});
+
+document.getElementById('forma-link').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.body.classList.add('fadeOut');
+  setTimeout(function () {
+    window.location.href = '../forma/forma.html';
+  }, 500);
+});
+
